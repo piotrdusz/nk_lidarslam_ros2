47142c9b0aec4baf67cddeb91c5b6fe98210a11c
Piotr Duszak <piotrdusz@yahoo.pl>
2024-01-20 23:11:59 +0100

    Dobranie parametrów

---
+++ lidarslam/launch/lidarslam_nk.launch.py
@@ -0,0 +1,67 @@
+import os
+
+import launch
+import launch_ros.actions
+
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+
+    main_param_dir = launch.substitutions.LaunchConfiguration(
+        'main_param_dir',
+        default=os.path.join(
+            get_package_share_directory('lidarslam'),
+            'param',
+            'lidarslam_nk.yaml'))
+    
+    rviz_param_dir = launch.substitutions.LaunchConfiguration(
+        'rviz_param_dir',
+        default=os.path.join(
+            get_package_share_directory('lidarslam'),
+            'rviz',
+            'mapping_nk.rviz'))
+
+    mapping = launch_ros.actions.Node(
+        package='scanmatcher',
+        executable='scanmatcher_node',
+        parameters=[main_param_dir],
+        remappings=[('/input_cloud','/ch128x1/lslidar_point_cloud')],
+        output='screen'
+        )
+
+    tf = launch_ros.actions.Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        arguments=[
+            '--frame-id', 'base_link',
+            '--child-frame-id', 'laser_link',
+            '--roll', '0.13',
+            '--yaw', '-1.57079632679'
+            ]
+        )
+
+
+    graphbasedslam = launch_ros.actions.Node(
+        package='graph_based_slam',
+        executable='graph_based_slam_node',
+        parameters=[main_param_dir],
+        output='screen'
+        )
+    
+    rviz = launch_ros.actions.Node(
+        package='rviz2',
+        executable='rviz2',
+        arguments=['-d', rviz_param_dir]
+        )
+
+
+    return launch.LaunchDescription([
+        launch.actions.DeclareLaunchArgument(
+            'main_param_dir',
+            default_value=main_param_dir,
+            description='Full path to main parameter file to load'),
+        mapping,
+        tf,
+        graphbasedslam,
+        rviz,
+            ])
